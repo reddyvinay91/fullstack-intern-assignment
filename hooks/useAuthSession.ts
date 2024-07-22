37ca42d286f5ab4d28d4314f064b9b1ca332e49d
reddyvinay91 <reddysrivinay01@gmail.com>
2024-07-22 19:26:12 +0530

    added session hook code and rest api code

--- hooks/useAuthSession.ts
+++ hooks/useAuthSession.ts
@@ -1,12 +1,27 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { setUser, clearAuth } from "@/redux/auth/auth.slice";
+import { setUser,setToken, clearAuth } from "@/redux/auth/auth.slice";
 import { RootState } from "@/redux/store";
 
 const useAuthSession = () => {
   const dispatch = useDispatch();
   const user = useSelector((state: RootState) => state.auth.user);
   //  implement the logic here to check user session
+  useEffect(() => {
+    // Example: Replace with actual logic to check if user session exists
+    const checkUserSession = () => {
+      const storedUser = localStorage.getItem("user");
+      const storedToken = localStorage.getItem("token");
+      if (storedUser && storedToken) {
+        dispatch(setUser(JSON.parse(storedUser)));
+        dispatch(setToken(storedToken));
+      } else {
+        dispatch(clearAuth());
+      }
+    };
+    checkUserSession();
+    
+  }, [dispatch]);
   return user;
 };
 
